ae68547c5c924d2bedb3a953349e7f31dcc0f6bb
Ashish Kumar Nayak <162096598+ashish0987aashish@users.noreply.github.com>
2024-11-03 21:26:19 +0530

    Update SignUp.jsx

--- frontend/travel-story-app/src/pages/Auth/SignUp.jsx
+++ frontend/travel-story-app/src/pages/Auth/SignUp.jsx
@@ -4,6 +4,7 @@ import PasswordInput from "../../components/Input/PasswordInput";
 import { useNavigate } from "react-router-dom";
 import { validateEmail } from "../../utils/helper";
 import axiosInstance from "../../utils/axiosInstance";
+import './SignUp.css'
 
 const SignUp = () => {
   const [name, setName] = useState("");
@@ -67,7 +68,7 @@ const SignUp = () => {
       <div className="login-ui-box bg-cyan-200 -bottom-40 right-1/2" />
 
       <div className="container h-screen flex items-center justify-center px-20 my-auto">
-        <div className="w-2/4 h-[90vh] flex items-end bg-signup-bg-image bg-cover bg-center rounded-lg p-10 z-50">
+        <div className="w-2/4 h-[90vh] flex items-end signup-bagha bg-cover bg-center rounded-lg p-10 z-50">
           <div>
             <h4 className="text-5xl text-white font-semibold leading-[58px]">
               Capture Your <br /> Journeys
